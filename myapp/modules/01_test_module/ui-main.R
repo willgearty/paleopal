30c672a2ca09ce0792ac577f8b38d86033d23695
William Gearty <willgearty@gmail.com>
2025-03-06 12:21:26 -0500

    Rename module directory

--- myapp/modules/01_test_module/ui-main.R
+++ myapp/modules/01_test_module/ui-main.R
@@ -1,7 +1,8 @@
-test_module_ui_main <- function() {
-  tagList(
+accordion_panel(
+  title = "Download Data",
+  card(
     actionButton(".add_option_1", "Download data"),
     actionButton(".add_option_2", "Save the data"),
-    actionButton(".add_option_3", "Explore the data"),
+    actionButton(".add_option_3", "Explore the data")
   )
-}
+)
