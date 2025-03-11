d5cc4d755f8680f45374cd9d9dc2af626193e5f9
William Gearty <willgearty@gmail.com>
2025-03-11 11:33:38 -0400

    Move old common components into new file

--- myapp/global.R
+++ myapp/global.R
@@ -33,17 +33,3 @@ library(deeptime)
 # get list of module directories
 # modules should be listed in the order they will be loaded (prepend numbers)
 modules <- list.dirs("./modules/", recursive = FALSE)
-
-# common UI components ####
-
-# a version of the accordion panel that includes a remove button
-accordion_panel_paleopal <- function(ind, ...) {
-  tmp <- accordion_panel(
-    ...,
-    actionButton(paste0(".remove_step_", ind), "Remove this step"),
-    value = paste0("step_", ind)
-  )
-  # need this attribute for sortable_js_capture_input
-  tmp$attribs$`data-rank-id` <- paste0("step_", ind)
-  tmp
-}
--- myapp/ui-common.R
+++ myapp/ui-common.R
@@ -1,3 +1,17 @@
+# common UI components ####
+
+# a version of the accordion panel that includes a remove button
+accordion_panel_paleopal <- function(ind, ...) {
+  tmp <- accordion_panel(
+    ...,
+    actionButton(paste0(".remove_step_", ind), "Remove this step"),
+    value = paste0("step_", ind)
+  )
+  # need this attribute for sortable_js_capture_input
+  tmp$attribs$`data-rank-id` <- paste0("step_", ind)
+  tmp
+}
+
 select_dataset_input <- function(ind) {
   df_names <- get_int_dfs()
   selectInput(paste0("dataset_", ind), "Choose a dataset:",
