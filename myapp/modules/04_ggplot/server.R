3f4d25d544cc308e55253caedbadb7247e6fbcad
William Gearty <willgearty@gmail.com>
2026-06-10 15:48:00 -0400

    Use add_shinypal_plot_step

--- myapp/modules/04_ggplot/server.R
+++ myapp/modules/04_ggplot/server.R
@@ -14,52 +14,38 @@ observeEvent(input$mod04_add_option_1, {
     get_int_data(input[[paste0("dataset_", ind)]])()
   })
 
-  # make a scatterplot with ggplot
-  output[[paste0("plot_", ind)]] <- metaRender2(renderPlot, {
-    df <- plot_df()
-    # leave the plot blank (via req) rather than repeating the validation
-    # message -- the code chunk shown just above already displays it
-    req(df[[input[[paste0("column_", ind, "_1")]]]],
-        df[[input[[paste0("column_", ind, "_2")]]]])
-    req(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
-        is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
-    metaExpr({
-      ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
-             aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
-                 y = !!..(input[[paste0("column_", ind, "_2")]]))) +
-        geom_point(color = "red") +
-        theme_classic()
-    })
-  })
-  output[[paste0("code_", ind)]] <- renderPrint({
-    df <- plot_df()
-    validate(need(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
-                  "x-axis column must be numeric"),
-             need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
-                  "y-axis column must be numeric"))
-    get_chunk(ind)
-  })
-
-  clip_observe(input, output, ind, expr(get_chunk(ind)))
-
-  # add the UI elements to the workflow and report
-  add_shinypal_step(
-    input, ind, mod04_ui_option_1, mod04_report_option_1,
-    list(
-      inject(quote(
-        output[[paste0("plot_", !!ind)]]()
-      ))
-    ),
-    c("ggplot2")
+  add_shinypal_plot_step(
+    input, output, ind,
+    # make a scatterplot with ggplot
+    plot = metaRender2(renderPlot, {
+      df <- plot_df()
+      # leave the plot blank (via req) rather than repeating the validation
+      # message -- the code chunk shown just above already displays it
+      req(df[[input[[paste0("column_", ind, "_1")]]]],
+          df[[input[[paste0("column_", ind, "_2")]]]])
+      req(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
+          is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
+      metaExpr({
+        ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
+               aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
+                   y = !!..(input[[paste0("column_", ind, "_2")]]))) +
+          geom_point(color = "red") +
+          theme_classic()
+      })
+    }),
+    fun_workflow = mod04_ui_option_1, fun_report = mod04_report_option_1,
+    libs = "ggplot2",
+    # surface validation messages before showing the generated code
+    code_guard = function() {
+      df <- plot_df()
+      validate(need(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
+                    "x-axis column must be numeric"),
+               need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
+                    "y-axis column must be numeric"))
+    },
+    column_ids = c(paste0("column_", ind, "_1"),
+                   paste0("column_", ind, "_2"))
   )
-
-  # choices should always include all intermediate data.frames
-  df_select_observe(input, ind)
-
-  # if chosen dataset is changed, change the column choices
-  column_select_observe(input, ind, paste0("column_", ind, "_1"))
-  column_select_observe(input, ind, paste0("column_", ind, "_2"))
-
 }, ignoreInit = TRUE)
 
 # handle adding the second option (map)
@@ -74,53 +60,41 @@ observeEvent(input$mod04_add_option_2, {
     get_int_data(input[[paste0("dataset_", ind)]])()
   })
 
-  # make a map of occs with ggplot
-  output[[paste0("map_", ind)]] <- metaRender2(renderPlot, {
-    df <- plot_df()
-    # leave the plot blank (via req) rather than repeating the validation
-    # message -- the code chunk shown just above already displays it
-    req(df[[input[[paste0("column_", ind, "_1")]]]],
-        df[[input[[paste0("column_", ind, "_2")]]]])
-    req(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
-        is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
-    metaExpr({
-      ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
-             aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
-                 y = !!..(input[[paste0("column_", ind, "_2")]]))) +
-        borders("world") +
-        geom_point(color = "red") +
-        coord_sf()
-    })
-  })
-  output[[paste0("code_", ind)]] <- renderPrint({
-    df <- plot_df()
-    validate(need(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
-                  "Longitude column must be numeric"),
-             need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
-                  "Latitude column must be numeric"))
-    get_chunk(ind)
-  })
-
-  clip_observe(input, output, ind, expr(get_chunk(ind)))
-
-  # add the UI elements to the workflow and report
-  add_shinypal_step(
-    input, ind, mod04_ui_option_2, mod04_report_option_2,
-    list(
-      inject(quote(
-        output[[paste0("map_", !!ind)]]()
-      ))
-    ),
-    c("ggplot2")
+  add_shinypal_plot_step(
+    input, output, ind,
+    # make a map of occs with ggplot
+    plot = metaRender2(renderPlot, {
+      df <- plot_df()
+      # leave the plot blank (via req) rather than repeating the validation
+      # message -- the code chunk shown just above already displays it
+      req(df[[input[[paste0("column_", ind, "_1")]]]],
+          df[[input[[paste0("column_", ind, "_2")]]]])
+      req(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
+          is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
+      metaExpr({
+        ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
+               aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
+                   y = !!..(input[[paste0("column_", ind, "_2")]]))) +
+          borders("world") +
+          geom_point(color = "red") +
+          coord_sf()
+      })
+    }),
+    fun_workflow = mod04_ui_option_2, fun_report = mod04_report_option_2,
+    libs = "ggplot2",
+    # the report renders this step into output$map_<ind>, not plot_<ind>
+    output_prefix = "map_",
+    # surface validation messages before showing the generated code
+    code_guard = function() {
+      df <- plot_df()
+      validate(need(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
+                    "Longitude column must be numeric"),
+               need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
+                    "Latitude column must be numeric"))
+    },
+    column_ids = c(paste0("column_", ind, "_1"),
+                   paste0("column_", ind, "_2"))
   )
-
-  # choices should always include all intermediate data.frames
-  df_select_observe(input, ind)
-
-  # if chosen dataset is changed, change the column choices
-  column_select_observe(input, ind, paste0("column_", ind, "_1"))
-  column_select_observe(input, ind, paste0("column_", ind, "_2"))
-
 }, ignoreInit = TRUE)
 
 # handle adding the third option (time series with a geological timescale)
@@ -135,70 +109,57 @@ observeEvent(input$mod04_add_option_3, {
     get_int_data(input[[paste0("dataset_", ind)]])()
   })
 
-  # plot a time series; the geom and the geological timescale axis are toggleable
-  output[[paste0("plot_", ind)]] <- metaRender2(renderPlot, {
-    df <- plot_df()
-    # leave the plot blank (via req) rather than repeating the validation
-    # message -- the code chunk shown just above already displays it
-    req(df[[input[[paste0("column_", ind, "_1")]]]],
-        df[[input[[paste0("column_", ind, "_2")]]]])
-    req(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
-        is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
-    # pick the geom from the dropdown (default to points if unset)
-    geom_layer <- if (identical(input[[paste0("geom_", ind)]], "line")) {
-      quote(geom_line())
-    } else {
-      quote(geom_point())
-    }
-    # assemble the layers, adding coord_geo() only when the axis box is checked
-    # and deeptime is available
-    layers <- list(geom_layer, quote(scale_x_reverse()))
-    if (isTRUE(input[[paste0("axis_", ind)]]) && deeptime_available) {
-      layers <- c(layers, list(quote(coord_geo())))
-    }
-    layers <- c(layers, list(quote(theme_classic())))
-    # build the ggplot() call then fold the layers on with `+`. constructing the
-    # expression (instead of a literal metaExpr) lets us omit coord_geo()
-    # entirely when the axis is off, rather than leaving a `+ NULL` stub; the
-    # ..()/!! markers are still expanded by metaExpr() below. the time column
-    # must be in millions of years (Ma) for the timescale to line up
-    plot_expr <- quote(
-      ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
-             aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
-                 y = !!..(input[[paste0("column_", ind, "_2")]])))
-    )
-    for (layer in layers) plot_expr <- call("+", plot_expr, layer)
-    metaExpr(plot_expr, quoted = TRUE)
-  })
-  output[[paste0("code_", ind)]] <- renderPrint({
-    df <- plot_df()
-    validate(need(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
-                  "Time column must be numeric (ages in Ma)"),
-             need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
-                  "y-axis column must be numeric"))
-    get_chunk(ind)
-  })
-
-  clip_observe(input, output, ind, expr(get_chunk(ind)))
-
-  # add the UI elements to the workflow and report
-  add_shinypal_step(
-    input, ind, mod04_ui_option_3, mod04_report_option_3,
-    list(
-      inject(quote(
-        output[[paste0("plot_", !!ind)]]()
-      ))
-    ),
-    if (deeptime_available) c("ggplot2", "deeptime") else "ggplot2"
+  add_shinypal_plot_step(
+    input, output, ind,
+    # plot a time series; the geom and the geological timescale axis are toggleable
+    plot = metaRender2(renderPlot, {
+      df <- plot_df()
+      # leave the plot blank (via req) rather than repeating the validation
+      # message -- the code chunk shown just above already displays it
+      req(df[[input[[paste0("column_", ind, "_1")]]]],
+          df[[input[[paste0("column_", ind, "_2")]]]])
+      req(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
+          is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
+      # pick the geom from the dropdown (default to points if unset)
+      geom_layer <- if (identical(input[[paste0("geom_", ind)]], "line")) {
+        quote(geom_line())
+      } else {
+        quote(geom_point())
+      }
+      # assemble the layers, adding coord_geo() only when the axis box is checked
+      # and deeptime is available
+      layers <- list(geom_layer, quote(scale_x_reverse()))
+      if (isTRUE(input[[paste0("axis_", ind)]]) && deeptime_available) {
+        layers <- c(layers, list(quote(coord_geo())))
+      }
+      layers <- c(layers, list(quote(theme_classic())))
+      # build the ggplot() call then fold the layers on with `+`. constructing the
+      # expression (instead of a literal metaExpr) lets us omit coord_geo()
+      # entirely when the axis is off, rather than leaving a `+ NULL` stub; the
+      # ..()/!! markers are still expanded by metaExpr() below. the time column
+      # must be in millions of years (Ma) for the timescale to line up
+      plot_expr <- quote(
+        ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
+               aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
+                   y = !!..(input[[paste0("column_", ind, "_2")]])))
+      )
+      for (layer in layers) plot_expr <- call("+", plot_expr, layer)
+      metaExpr(plot_expr, quoted = TRUE)
+    }),
+    fun_workflow = mod04_ui_option_3, fun_report = mod04_report_option_3,
+    # coord_geo() comes from deeptime, so only require it when available
+    libs = if (deeptime_available) c("ggplot2", "deeptime") else "ggplot2",
+    # surface validation messages before showing the generated code
+    code_guard = function() {
+      df <- plot_df()
+      validate(need(is.numeric(df[[input[[paste0("column_", ind, "_1")]]]]),
+                    "Time column must be numeric (ages in Ma)"),
+               need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
+                    "y-axis column must be numeric"))
+    },
+    column_ids = c(paste0("column_", ind, "_1"),
+                   paste0("column_", ind, "_2"))
   )
-
-  # choices should always include all intermediate data.frames
-  df_select_observe(input, ind)
-
-  # if chosen dataset is changed, change the column choices
-  column_select_observe(input, ind, paste0("column_", ind, "_1"))
-  column_select_observe(input, ind, paste0("column_", ind, "_2"))
-
 }, ignoreInit = TRUE)
 
 # handle adding the fourth option (boxplot)
@@ -213,63 +174,49 @@ observeEvent(input$mod04_add_option_4, {
     get_int_data(input[[paste0("dataset_", ind)]])()
   })
 
-  # make a box or violin plot: a numeric y-axis grouped by a categorical x-axis
-  output[[paste0("plot_", ind)]] <- metaRender2(renderPlot, {
-    df <- plot_df()
-    # leave the plot blank (via req) rather than repeating the validation
-    # message -- the code chunk shown just above already displays it
-    req(df[[input[[paste0("column_", ind, "_1")]]]],
-        df[[input[[paste0("column_", ind, "_2")]]]])
-    req(is.character(df[[input[[paste0("column_", ind, "_1")]]]]) ||
-          is.factor(df[[input[[paste0("column_", ind, "_1")]]]]),
-        is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
-    # pick the geom from the dropdown (default to a boxplot)
-    geom_layer <- if (identical(input[[paste0("geom_", ind)]], "violin")) {
-      quote(geom_violin())
-    } else {
-      quote(geom_boxplot())
-    }
-    # build the ggplot() call then fold on the chosen geom and theme; see the
-    # time-series step for why we construct the expression instead of writing a
-    # literal metaExpr (the ..()/!! markers are still expanded by metaExpr below)
-    plot_expr <- quote(
-      ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
-             aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
-                 y = !!..(input[[paste0("column_", ind, "_2")]])))
-    )
-    for (layer in list(geom_layer, quote(theme_classic()))) {
-      plot_expr <- call("+", plot_expr, layer)
-    }
-    metaExpr(plot_expr, quoted = TRUE)
-  })
-  output[[paste0("code_", ind)]] <- renderPrint({
-    df <- plot_df()
-    validate(need(is.character(df[[input[[paste0("column_", ind, "_1")]]]]) ||
-                    is.factor(df[[input[[paste0("column_", ind, "_1")]]]]),
-                  "Grouping column must be categorical (text or factor)"),
-             need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
-                  "y-axis column must be numeric"))
-    get_chunk(ind)
-  })
-
-  clip_observe(input, output, ind, expr(get_chunk(ind)))
-
-  # add the UI elements to the workflow and report
-  add_shinypal_step(
-    input, ind, mod04_ui_option_4, mod04_report_option_4,
-    list(
-      inject(quote(
-        output[[paste0("plot_", !!ind)]]()
-      ))
-    ),
-    c("ggplot2")
+  add_shinypal_plot_step(
+    input, output, ind,
+    # make a box or violin plot: a numeric y-axis grouped by a categorical x-axis
+    plot = metaRender2(renderPlot, {
+      df <- plot_df()
+      # leave the plot blank (via req) rather than repeating the validation
+      # message -- the code chunk shown just above already displays it
+      req(df[[input[[paste0("column_", ind, "_1")]]]],
+          df[[input[[paste0("column_", ind, "_2")]]]])
+      req(is.character(df[[input[[paste0("column_", ind, "_1")]]]]) ||
+            is.factor(df[[input[[paste0("column_", ind, "_1")]]]]),
+          is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]))
+      # pick the geom from the dropdown (default to a boxplot)
+      geom_layer <- if (identical(input[[paste0("geom_", ind)]], "violin")) {
+        quote(geom_violin())
+      } else {
+        quote(geom_boxplot())
+      }
+      # build the ggplot() call then fold on the chosen geom and theme; see the
+      # time-series step for why we construct the expression instead of writing a
+      # literal metaExpr (the ..()/!! markers are still expanded by metaExpr below)
+      plot_expr <- quote(
+        ggplot(..(get_int_data(input[[paste0("dataset_", ind)]])()),
+               aes(x = !!..(input[[paste0("column_", ind, "_1")]]),
+                   y = !!..(input[[paste0("column_", ind, "_2")]])))
+      )
+      for (layer in list(geom_layer, quote(theme_classic()))) {
+        plot_expr <- call("+", plot_expr, layer)
+      }
+      metaExpr(plot_expr, quoted = TRUE)
+    }),
+    fun_workflow = mod04_ui_option_4, fun_report = mod04_report_option_4,
+    libs = "ggplot2",
+    # surface validation messages before showing the generated code
+    code_guard = function() {
+      df <- plot_df()
+      validate(need(is.character(df[[input[[paste0("column_", ind, "_1")]]]]) ||
+                      is.factor(df[[input[[paste0("column_", ind, "_1")]]]]),
+                    "Grouping column must be categorical (text or factor)"),
+               need(is.numeric(df[[input[[paste0("column_", ind, "_2")]]]]),
+                    "y-axis column must be numeric"))
+    },
+    column_ids = c(paste0("column_", ind, "_1"),
+                   paste0("column_", ind, "_2"))
   )
-
-  # choices should always include all intermediate data.frames
-  df_select_observe(input, ind)
-
-  # if chosen dataset is changed, change the column choices
-  column_select_observe(input, ind, paste0("column_", ind, "_1"))
-  column_select_observe(input, ind, paste0("column_", ind, "_2"))
-
 }, ignoreInit = TRUE)
